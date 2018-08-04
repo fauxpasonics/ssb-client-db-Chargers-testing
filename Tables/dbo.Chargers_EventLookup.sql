99bff9668f785ae0c4809bb6e29065398013e5d8
Luke Miles <lmiles@ssbinfo.com>
2018-08-03 20:02:02 -0400

    Initial Commit

---
+++ Tables/dbo.Chargers_EventLookup.sql
@@ -0,0 +1,7 @@
+CREATE TABLE [dbo].[Chargers_EventLookup]
+(
+[season_year] [varchar] (25) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+[event_name] [varchar] (50) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+[event_id] [varchar] (10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL
+)
+GO
